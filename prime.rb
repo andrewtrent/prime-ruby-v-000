976a69e61e457715ed8c9fb392fd35811a93fac0
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-16 23:34:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,10 +5,12 @@ def prime?(n)
          return true
       else if n % 2 == 0 || n % 3 == 0
          return false
-      let i = 5
+      i = 5
       while i * i <= n
          if n % i == 0 or n % (i + 2) == 0
              return false
          i = i + 6
+        end
+      end
       return true
 end
