5f19ef8dd8719906eefd68f4c1e75adda6a59ccf
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-16 23:33:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
-def prime?(number_given)
-  i = number_given - 1
-  while i > 1
-    (number_given / i).odd? || (nummber_given / i).even?
-  end
+def prime?(n)
+      if n <= 1
+         return false
+      else if n <= 3
+         return true
+      else if n % 2 == 0 || n % 3 == 0
+         return false
+      let i = 5
+      while i * i <= n
+         if n % i == 0 or n % (i + 2) == 0
+             return false
+         i = i + 6
+      return true
 end
