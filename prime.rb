af82270b1c25587f2dcb7863037cda8550917aa7
Andrew Trent <andrewtrentdesign@gmail.com>
2018-10-16 23:35:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(n)
       if n <= 1
          return false
-      else if n <= 3
+      elsif n <= 3
          return true
-      else if n % 2 == 0 || n % 3 == 0
+      elsif n % 2 == 0 || n % 3 == 0
          return false
       i = 5
       while i * i <= n
